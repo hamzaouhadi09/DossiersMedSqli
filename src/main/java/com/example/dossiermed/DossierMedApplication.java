58b31d78066d0f3bb03e2eb8488d09460fd619c7
marouane <marouane.qaffssaoui@uit.ac.ma>
2024-06-06 12:50:34 +0100

    initial commit

---
+++ src/main/java/com/example/dossiermed/DossierMedApplication.java
@@ -0,0 +1,13 @@
+package com.example.dossiermed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DossierMedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DossierMedApplication.class, args);
+    }
+
+}
